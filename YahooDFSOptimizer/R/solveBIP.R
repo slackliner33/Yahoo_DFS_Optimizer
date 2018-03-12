69411e2c5efb70c457b916f188cdc6204efed821
Jake Johnson <alpineslalomracer33@gmail.com>
2018-03-12 05:05:42 -0400

    UPDATES

--- YahooDFSOptimizer/R/solveBIP.R
+++ YahooDFSOptimizer/R/solveBIP.R
@@ -1,6 +1,16 @@
-#BINARY INTEGER PROGRAM MODEL SET UP
+#' Optimize function
+#'
+#' This function generates 10 best lineups based off the Rotowire NBA DFS
+#' predictions for the given day of NBA games
+#' @param Defaults to TRUE
+#' @keywords
+#' @export
+#' @examples
+#' generateLineups()
 
-require(lpSolve)
+generateLineups <- function(){
+
+#Restructures data and preps for Binary IP
 PG <- DFS_Data[which(DFS_Data$Position == "PG"),]
 SG <- DFS_Data[which(DFS_Data$Position == "SG"),]
 SF <- DFS_Data[which(DFS_Data$Position == "SF"),]
@@ -14,6 +24,7 @@ SF_len <- nrow(SF)
 PF_len <- nrow(PF)
 C_len <- nrow(C)
 
+#creation of constraint matrix
 row1 <- rep(0, DFS_len)
 row1[1:PG_len] <- 1
 row2 <- rep(0, DFS_len)
@@ -31,7 +42,7 @@ row7[(PG_len+SG_len+SF_len+PF_len+1):DFS_len] <- 1
 row8 <- rep(1,DFS_len)
 row9 <- DFS$Salary
 
-#model info / solving
+#IP model set-up / solving
 constraints <- matrix(rbind(row1,row2,row3,row4,row5,row6,row7,row8,row9), nrow = 9, byrow = FALSE)
 obj_fn <- DFS$Projection
 RHS <- c(1,1,3,1,1,3,1,8,200)
@@ -39,11 +50,12 @@ ineq <- c(">=", ">=", ">=", ">=", ">=", ">=",">=","=","<=")
 
 my_lp <- lp("max", obj_fn, constraints, ineq, RHS,
                         compute.sens = TRUE, all.bin=TRUE)
-#results
-my_lp
-my_lp$solution
+#   results
 
-my_lp$num.bin.solns
+#my_lp
+#my_lp$solution
+
+#my_lp$num.bin.solns
 
 soln_vec <- my_lp$solution
 
@@ -52,6 +64,7 @@ opt_name <- rep("name", 8)
 
 Players <- DFS$Name
 
+#matches IP solution to specific player names
 counter <- 1
 for (i in 1:length(soln_vec)) {
   if (soln_vec[i]==1) {
@@ -63,10 +76,9 @@ for (i in 1:length(soln_vec)) {
 optimal_lineup <- as.data.frame(cbind(opt_pos,opt_name))
 colnames(optimal_lineup) <- c("Position", "Name")
 
-#optimal Yahoo answer
-#optimal_lineup
-
-best_val <- my_lp$objval
+#addition of constraint (optimal - 0.1) to force IP to select next best
+#solution... Iterative process /code to generate next 9 lineups
+best_val <- round(my_lp$objval,1)
 new_limit <- best_val - 0.1
 
 RHS_2 <- c(RHS,new_limit)
@@ -76,11 +88,10 @@ ineq2 <- c(">=", ">=", ">=", ">=", ">=", ">=",">=","=","<=","<=")
 
 my_lp2 <- lp("max", obj_fn, constraints2, ineq2, RHS_2,
             compute.sens = TRUE, all.bin=TRUE)
-#results
-my_lp2
-my_lp2$solution
+#my_lp2
+#my_lp2$solution
 
-my_lp2$num.bin.solns
+#my_lp2$num.bin.solns
 
 soln_vec <- my_lp2$solution
 
@@ -95,16 +106,14 @@ for (i in 1:length(soln_vec)) {
 optimal_lineup2 <- as.data.frame(cbind(opt_pos,opt_name))
 colnames(optimal_lineup2) <- c("Position", "Name")
 
-best_val2 <- my_lp2$objval
+best_val2 <- round(my_lp2$objval,1)
 RHS_2[10] <- best_val2 - 0.1
 
 my_lp3 <- lp("max", obj_fn, constraints2, ineq2, RHS_2,
              compute.sens = TRUE, all.bin=TRUE)
-#results
-my_lp3
-my_lp3$solution
-
-my_lp3$num.bin.solns
+#my_lp3
+#my_lp3$solution
+#my_lp3$num.bin.solns
 
 soln_vec <- my_lp3$solution
 
@@ -119,16 +128,14 @@ for (i in 1:length(soln_vec)) {
 optimal_lineup3 <- as.data.frame(cbind(opt_pos,opt_name))
 colnames(optimal_lineup3) <- c("Position", "Name")
 
-best_val3 <- my_lp3$objval
+best_val3 <- round(my_lp3$objval,1)
 RHS_2[10] <- best_val3 - 0.1
 
 my_lp4 <- lp("max", obj_fn, constraints2, ineq2, RHS_2,
              compute.sens = TRUE, all.bin=TRUE)
-#results
-my_lp4
-my_lp4$solution
-
-my_lp4$num.bin.solns
+#my_lp4
+#my_lp4$solution
+#my_lp4$num.bin.solns
 
 soln_vec <- my_lp4$solution
 
@@ -143,17 +150,14 @@ for (i in 1:length(soln_vec)) {
 optimal_lineup4 <- as.data.frame(cbind(opt_pos,opt_name))
 colnames(optimal_lineup4) <- c("Position", "Name")
 
-best_val4 <- my_lp4$objval
+best_val4 <- round(my_lp4$objval,1)
 RHS_2[10] <- best_val4 - 0.1
 
 my_lp5 <- lp("max", obj_fn, constraints2, ineq2, RHS_2,
              compute.sens = TRUE, all.bin=TRUE)
-#results
-my_lp5
-my_lp5$solution
-best_val5<-my_lp5$objval
-
-my_lp5$num.bin.solns
+#my_lp5
+#my_lp5$solution
+#my_lp5$num.bin.solns
 
 soln_vec <- my_lp5$solution
 
@@ -168,32 +172,152 @@ for (i in 1:length(soln_vec)) {
 optimal_lineup5 <- as.data.frame(cbind(opt_pos,opt_name))
 colnames(optimal_lineup5) <- c("Position", "Name")
 
+best_val5 <- round(my_lp5$objval,1)
+RHS_2[10] <- best_val5 - 0.1
+
+my_lp6 <- lp("max", obj_fn, constraints2, ineq2, RHS_2,
+             compute.sens = TRUE, all.bin=TRUE)
+#my_lp6
+#my_lp6$solution
+#my_lp6$num.bin.solns
+
+soln_vec <- my_lp6$solution
+
+counter <- 1
+for (i in 1:length(soln_vec)) {
+  if (soln_vec[i]==1) {
+    opt_name[counter] <- Players[i]
+    counter <- counter + 1
+    next
+  }
+}
+optimal_lineup6 <- as.data.frame(cbind(opt_pos,opt_name))
+colnames(optimal_lineup6) <- c("Position", "Name")
+
+best_val6 <- round(my_lp6$objval,1)
+RHS_2[10] <- best_val6 - 0.1
+
+my_lp7 <- lp("max", obj_fn, constraints2, ineq2, RHS_2,
+             compute.sens = TRUE, all.bin=TRUE)
+#my_lp7
+#my_lp7$solution
+#my_lp7$num.bin.solns
+
+soln_vec <- my_lp7$solution
+
+counter <- 1
+for (i in 1:length(soln_vec)) {
+  if (soln_vec[i]==1) {
+    opt_name[counter] <- Players[i]
+    counter <- counter + 1
+    next
+  }
+}
+optimal_lineup7 <- as.data.frame(cbind(opt_pos,opt_name))
+colnames(optimal_lineup7) <- c("Position", "Name")
+
+best_val7 <- round(my_lp7$objval,1)
+RHS_2[10] <- best_val7 - 0.1
+
+my_lp8 <- lp("max", obj_fn, constraints2, ineq2, RHS_2,
+             compute.sens = TRUE, all.bin=TRUE)
+#my_lp8
+#my_lp8$solution
+#my_lp8$num.bin.solns
+
+soln_vec <- my_lp8$solution
+
+counter <- 1
+for (i in 1:length(soln_vec)) {
+  if (soln_vec[i]==1) {
+    opt_name[counter] <- Players[i]
+    counter <- counter + 1
+    next
+  }
+}
+optimal_lineup8 <- as.data.frame(cbind(opt_pos,opt_name))
+colnames(optimal_lineup8) <- c("Position", "Name")
+
+best_val8 <- round(my_lp8$objval, 1)
+RHS_2[10] <- best_val8 - 0.1
+
+my_lp9 <- lp("max", obj_fn, constraints2, ineq2, RHS_2,
+             compute.sens = TRUE, all.bin=TRUE)
+#my_lp9
+#my_lp9$solution
+#my_lp9$num.bin.solns
+
+soln_vec <- my_lp9$solution
+
+counter <- 1
+for (i in 1:length(soln_vec)) {
+  if (soln_vec[i]==1) {
+    opt_name[counter] <- Players[i]
+    counter <- counter + 1
+    next
+  }
+}
+optimal_lineup9 <- as.data.frame(cbind(opt_pos,opt_name))
+colnames(optimal_lineup9) <- c("Position", "Name")
+
+best_val9 <- round(my_lp9$objval,1)
+RHS_2[10] <- best_val9 - 0.1
+
+my_lp10 <- lp("max", obj_fn, constraints2, ineq2, RHS_2,
+             compute.sens = TRUE, all.bin=TRUE)
+#my_lp10
+#my_lp10$solution
+#my_lp10$num.bin.solns
+best_val10<- round(my_lp10$objval,1)
+
+soln_vec <- my_lp10$solution
+
+counter <- 1
+for (i in 1:length(soln_vec)) {
+  if (soln_vec[i]==1) {
+    opt_name[counter] <- Players[i]
+    counter <- counter + 1
+    next
+  }
+}
+optimal_lineup10 <- as.data.frame(cbind(opt_pos,opt_name))
+colnames(optimal_lineup10) <- c("Position", "Name")
+
+#determining costs of each line-up
 cost <- sum(my_lp$solution*row9)
 cost2 <- sum(my_lp2$solution*row9)
 cost3 <- sum(my_lp3$solution*row9)
 cost4<- sum(my_lp4$solution*row9)
 cost5<- sum(my_lp5$solution*row9)
+cost6 <- sum(my_lp6$solution*row9)
+cost7 <- sum(my_lp7$solution*row9)
+cost8 <- sum(my_lp8$solution*row9)
+cost9<- sum(my_lp9$solution*row9)
+cost10<- sum(my_lp10$solution*row9)
 
 
-#FINAL RESULTS
-#optimal Yahoo line-ups (top 5)
-optimal_lineup
-best_val
-cost
+#FINAL RESULTS, OUTPUT
+#optimal Yahoo line-ups (top 10)
 
-optimal_lineup2
-best_val2
-cost2
+Best_10_Lineups <- cbind(optimal_lineup,optimal_lineup2$Name,optimal_lineup3$Name,
+      optimal_lineup4$Name,optimal_lineup5$Name,optimal_lineup6$Name,
+      optimal_lineup7$Name,optimal_lineup8$Name,optimal_lineup9$Name,
+      optimal_lineup10$Name)
+colnames(Best_10_Lineups) <- (c("Position","Optimal","2nd Best","3rd Best",
+                                "4th Best","5th Best","6th Best","7th Best",
+                                "8th Best","9th Best", "10th Best"))
+projected_totals <- as.data.frame(t(c("Proj Pts",best_val,best_val2,best_val3,best_val4,best_val5,
+                      best_val6,best_val7,best_val8,best_val9,best_val10)))
+costs <- as.data.frame(t(c("Cost $",cost,cost2,cost3,cost4,cost5,cost6,cost7,cost8,cost9, cost10)))
+colnames(projected_totals) <- (c("Position","Optimal","2nd Best","3rd Best",
+                                 "4th Best","5th Best","6th Best","7th Best",
+                                 "8th Best","9th Best", "10th Best"))
+colnames(costs) <- (c("Position","Optimal","2nd Best","3rd Best",
+                                 "4th Best","5th Best","6th Best","7th Best",
+                                 "8th Best","9th Best", "10th Best"))
 
-optimal_lineup3
-best_val3
-cost3
+Best_10_Lineups <- rbind(Best_10_Lineups,projected_totals,costs)
 
-optimal_lineup4
-best_val4
-cost4
+Best_10_Lineups <<- Best_10_Lineups
 
-optimal_lineup5
-best_val5
-cost5
-
+}
